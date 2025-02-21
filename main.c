9fbf6472d4bdc57c65fe846eb96389d764f10603
0xfarco <0xfarco@gmail.com>
2025-02-21 07:12:42 +0530

    define registers

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX]; // 65536 memory locations
 
+enum
+{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC, /* program counter */
+    R_COND,
+    R_COUNT
+};
+
+uint16_t reg[R_COUNT];
+
 int main() {
     return 0;
 }
